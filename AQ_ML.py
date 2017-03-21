76628f1e7cabd23751209f580ebec7ba8571e741
Ruth, Daniel <Daniel.Ruth@nrel.gov>
2017-03-21 16:44:54 -0600

    consolidating files to module and "pipeline" file

--- AQ_ML.py
+++ AQ_ML.py
@@ -7,11 +7,15 @@ Created on Wed Mar 01 19:14:39 2017
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 #import pickle
 
+station_df_path = 'C:\Users\druth\Documents\FS\AirQuality\\aqs_monitors.csv'
+all_data_path = 'C:\Users\druth\Documents\FS\AirQuality\\daily_81102_allYears.csv'
 
-#station_df = pd.read_csv('aqs_monitors.csv')
-#all_data = pd.read_csv('daily_81102_allYears.csv',usecols=['State Code','County Code','Site Num','Date Local','Arithmetic Mean'])
+
+#station_df = pd.read_csv()
+#all_data = pd.read_csv(,usecols=['State Code','County Code','Site Num','Date Local','Arithmetic Mean'])
 #all_data = all_data.rename(columns={'Site Num':'Site Number'})
 
 
@@ -44,7 +48,26 @@ def lat_lon_dist(point1,point2):
     
     return d
     
-
+def identify_sampling_rate(series):
+    is_nan = pd.isnull(series)
+        
+    good_dates = series.index[is_nan==False]
+    diff_period = pd.Series(index=good_dates[0:-1],data=(good_dates[1:]-good_dates[0:-1]))
+    
+    print(diff_period)
+    
+    '''
+    plt.figure()
+    plt.plot(diff_period/pd.Timedelta('1d'),'o')
+    plt.show()
+    '''
+    
+    estimated_rate = diff_period.median()
+    
+    return estimated_rate
+    
+    
+'''
 def identify_sampling_rate(station_id,all_data,start_date=None,end_date=None):
     
     all_data = addon_stationid(all_data)
@@ -53,6 +76,7 @@ def identify_sampling_rate(station_id,all_data,start_date=None,end_date=None):
     station_data = station_data.set_index('Date Local')
     
     return station_data
+'''
 
 # with a given latlon and r_max, pick out stations within that radius from a df
 # with STATION DATA, not the metadata spreadsheet. This way we actually get 
@@ -68,7 +92,6 @@ def identify_nearby_stations(latlon,r_max,df):
 
     # compute distance between these sites and our point
     d = lat_lon_dist([param_stations['Latitude'],param_stations['Longitude']],[my_lat,my_lon])
-    print(d)
     param_stations['Distance'] = d
     param_stations = param_stations.sort_values(['Distance'],ascending=True)
     
@@ -84,7 +107,6 @@ def addon_stationid(df):
     station_ids = pd.Series(index=df.index)
     for i in station_ids.index:
         station_ids.ix[i] = str(df.ix[i]['State Code'])+'_'+str(df.ix[i]['County Code'])+'_'+str(df.ix[i]['Site Number'])
-        print(i,len(station_ids.index))
     df['station_ids'] = station_ids    
     
     return df
@@ -101,8 +123,13 @@ def remove_dup_stations(param_stations):
     return param_stations
     
 # pick out the values from stations nearby
-def extract_nearby_values(stations,all_data):
+def extract_nearby_values(stations,all_data,start_date,end_date):
     
+    #times = pd.date_range(start=start_date,end=end_date)
+    
+    df = pd.DataFrame()
+    
+    # collect data for each nearby station
     for idx in stations.index:
         
         county_code = stations.loc[idx]['County Code']
@@ -110,6 +137,41 @@ def extract_nearby_values(stations,all_data):
         site_number = stations.loc[idx]['Site Number']
         
         site_rawdata = all_data[(all_data['County Code']==county_code)&(all_data['State Code']==state_code)&(all_data['Site Number']==site_number)]
-        site_rawdata = site_rawdata.set_index(site_rawdata['Date Local'])
+        site_rawdata = site_rawdata.set_index(pd.to_datetime(site_rawdata['Date Local']))
+        
+        site_rawdata = site_rawdata[(site_rawdata.index>=start_date)&(site_rawdata.index<=end_date)]
+        
+        site_rawdata = site_rawdata[~site_rawdata.index.duplicated(keep='first')]
         site_series = pd.Series(index=site_rawdata.index,data=site_rawdata['Arithmetic Mean'])
         site_series = site_series.rename(idx)
+        
+        df = pd.concat([df,site_series],axis=1)
+        
+    return df
+    
+    
+def split_fill_unfill_stations(df):
+    
+    good_stations = pd.DataFrame()
+    bad_stations = pd.DataFrame()
+    for column in df:
+        col_vals = df[column]
+        rate = identify_sampling_rate(col_vals)
+        num_missing = len(col_vals[pd.isnull(col_vals)==True])
+        portion_missing = float(num_missing)/float(len(col_vals))
+        print(num_missing,len(col_vals),portion_missing)
+  
+        enough_data = (rate==pd.Timedelta('1d')) & (portion_missing < 0.1)
+        if enough_data:
+            good_stations = pd.concat([good_stations,col_vals],axis=1)
+        else:
+            bad_stations = pd.concat([bad_stations,col_vals],axis=1)
+            
+    return good_stations, bad_stations
+        
+def create_model_for_site(predictors,site):
+    
+    import sklearn.linear_model
+
+    linear_model = sklearn.linear_model.LinearRegression()
+    
--- aq_pipeline.py
+++ aq_pipeline.py
@@ -7,21 +7,41 @@ Created on Sun Mar 12 19:07:18 2017
 
 import AQ_ML as aq
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+#import numpy as np
 
-station_df = pd.read_csv('aqs_monitors.csv')
+#station_df = pd.read_csv(aq.station_df_path)
+#all_data = pd.read_csv(aq.all_data_path,usecols=['State Code','County Code','Site Num','Date Local','Arithmetic Mean','Parameter Code','Latitude','Longitude'])
+#all_data = all_data.rename(columns={'Site Num':'Site Number'})
 
-all_data = pd.read_csv('daily_81102_allYears.csv',usecols=['State Code','County Code','Site Num','Date Local','Arithmetic Mean','Parameter Code','Latitude','Longitude'])
-all_data = all_data.rename(columns={'Site Num':'Site Number'})
 
-latlon = (30,-90)
+latlon = (33.424564, -111.928001) # ASU
 r_max = 100
 
-start_date = '2012-01-01'
+start_date = '2012-03-01'
 end_date = '2013-01-01'
 
-station_data = pd.DataFrame(index=pd.date_range(start_date,end_date,freq='1D').date)
+#station_data = pd.DataFrame(index=pd.date_range(start_date,end_date,freq='1D').date)
 
+'''
 stations = aq.identify_nearby_stations(latlon,r_max,all_data)
+stations = aq.addon_stationid(stations)
+stations = aq.remove_dup_stations(stations)
+'''
 
-nearby_data = aq.extract_nearby_stations(stations,all_data)
+
+#nearby_data = aq.extract_nearby_values(stations,all_data,start_date,end_date)
+
+# split up the stations in to good stations (enough data) and bad ones (to be imputed)
+gs,bs = aq.split_fill_unfill_stations(nearby_data)
+
+# show the good and bad stations
+fig = plt.figure()
+ax_g = fig.add_subplot(2,1,1)
+ax_g.matshow(gs.transpose())
+ax_b = fig.add_subplot(2,1,2)
+ax_b.matshow(bs.transpose())
+
+# fill in missing data in each "bad column"
+for column in bs:
+    
